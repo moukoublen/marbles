01dd38cadfd24e7197ee2086354d67bf3b840d47
Kostas Stamatakis <moukoublen@gmail.com>
2020-02-09 01:38:27 +0200

    downloader

--- pkg/downloader/downloader.go
+++ pkg/downloader/downloader.go
@@ -4,7 +4,42 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
+
+func filenameFromURL(url string) string {
+	a := strings.Split(url, "/")
+	if a == nil || len(a) == 0 {
+		return ""
+	}
+	return a[len(a)-1]
+}
+
+var rgxFilename = regexp.MustCompile(`filename="(.*)"`)
+
+func filenameFromContentDisposition(cd string) string {
+	r := rgxFilename.FindStringSubmatch(cd)
+	if r != nil && len(r) >= 2 {
+		return r[1]
+	}
+	return ""
+}
+
+func filenameFromHeader(url string) string {
+	resp, err := http.Head(url)
+	if err != nil {
+		return ""
+	}
+
+	n := resp.Header.Get("content-disposition")
+	return n
+}
+
+// GetFilename returns the filename of file to download.
+func GetFilename(url string) string {
+	return ""
+}
 
 //Download function
 func Download(filepath string, url string) error {
---
+++ pkg/downloader/downloader_test.go
@@ -0,0 +1,49 @@
+package downloader
+
+import (
+	"testing"
+)
+
+func TestGetFilenameFromURL(t *testing.T) {
+	var testcases = []struct {
+		url              string
+		expectedFilename string
+	}{
+		{"", ""},
+		{"https://download.internet.o/files/file1.tar.bz2", "file1.tar.bz2"},
+		{"https://download.internet.o/files/", ""},
+		{"file", ""},
+	}
+
+	for _, testcase := range testcases {
+		tc := testcase
+		t.Run(tc.url, func(t *testing.T) {
+			t.Parallel()
+			n := filenameFromURL(tc.url)
+			if n != tc.expectedFilename {
+				t.Errorf("getFilenameFromURL failed for url %s. Expected %s got %s", tc.url, tc.expectedFilename, n)
+			}
+		})
+	}
+}
+
+func TestFilenameFromContentDisposition(t *testing.T) {
+	var testcases = []struct {
+		url              string
+		expectedFilename string
+	}{
+		{`filename="filename.jpg"`, "filename.jpg"},
+		{`nope`, ""},
+	}
+
+	for _, testcase := range testcases {
+		tc := testcase
+		t.Run(tc.url, func(t *testing.T) {
+			t.Parallel()
+			n := filenameFromContentDisposition(tc.url)
+			if n != tc.expectedFilename {
+				t.Errorf("getFilenameFromURL failed for url %s. Expected %s got %s", tc.url, tc.expectedFilename, n)
+			}
+		})
+	}
+}
